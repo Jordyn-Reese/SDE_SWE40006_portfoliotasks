24651e20b84a097bca26b0d93c56167fd6f8260f
Jordyn-Reese <104005067@student.swin.edu.au>
2026-08-18 14:10:41 +1000

    1.1
    Included all files from portfolio task 1.1

---
+++ 1.1/TodoListApp/TodoListApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TodoListApp
+{
+    class Todo
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is the first deployment task completed");
+            Console.WriteLine("Press a button to exit :D");
+            Console.ReadKey();
+        }
+    }
+}
